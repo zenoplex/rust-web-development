7b4c9c0ff412b86489eebb66f3777060e18f1a28
Gen Ichino <zenoplex@gmail.com>
2023-07-29 22:56:55 +0900

    feat: add basic setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    let hello = warp::get()
+        .and(warp::path("hello"))
+        .map(|| "Hello, world!");
+
+    warp::serve(hello)
+        .run(([127, 0, 0, 1], 3030))
+        .await;
 }
