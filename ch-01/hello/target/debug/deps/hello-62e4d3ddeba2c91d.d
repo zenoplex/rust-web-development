abc7a90a7363425a2c54a69b2c2718340082dc2d
Gen Ichino <zenoplex@gmail.com>
2023-07-30 16:10:10 +0900

    chore: remove workspace

--- ch-01/hello/target/debug/deps/hello-62e4d3ddeba2c91d.d
+++
@@ -1,5 +0,0 @@
-/Users/gen/projects/github.com/zenoplex/rust-web-development/hello/target/debug/deps/hello-62e4d3ddeba2c91d: src/main.rs
-
-/Users/gen/projects/github.com/zenoplex/rust-web-development/hello/target/debug/deps/hello-62e4d3ddeba2c91d.d: src/main.rs
-
-src/main.rs:
--- ch-01/hello/target/debug/hello.d
+++
@@ -1,1 +0,0 @@
-/Users/gen/projects/github.com/zenoplex/rust-web-development/hello/target/debug/hello: /Users/gen/projects/github.com/zenoplex/rust-web-development/hello/src/main.rs
