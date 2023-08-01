130acc072329ef6139b2f16a1601cfd12dc50a0f
Gen Ichino <zenoplex@gmail.com>
2023-08-02 00:48:59 +0900

    feat: add put operation

--- src/main.rs
+++ src/main.rs
@@ -163,8 +163,17 @@ async fn main() {
         .and(warp::body::json())
         .and_then(add_question);
 
+    let update_question = warp::put()
+        .and(warp::path("questions"))
+        .and(warp::path::param::<String>())
+        .and(warp::path::end())
+        .and(store_filter.clone())
+        .and(warp::body::json())
+        .and_then(update_question);
+
     let routes = get_questions
         .or(add_question)
+        .or(update_question)
         .with(cors)
         .recover(return_error);
 
