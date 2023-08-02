a210c512b3e4c55bf6912b3b4801f0de256b35bb
Gen Ichino <zenoplex@gmail.com>
2023-08-03 00:06:34 +0900

    feat: add add_answer fn

--- src/main.rs
+++ src/main.rs
@@ -145,6 +145,25 @@ async fn delete_question(id: String, store: Store) -> Result<impl Reply, Rejecti
     }
 }
 
+async fn add_answer(
+    store: Store,
+    params: HashMap<String, String>,
+) -> Result<impl Reply, Rejection> {
+    let answer = Answer {
+        id: AnswerId("1".to_string()),
+        content: params.get("content").unwrap().to_string(),
+        question_id: QuestionId(params.get("question_id").unwrap().to_string()),
+    };
+
+    store
+        .answers
+        .write()
+        .await
+        .insert(answer.id.clone(), answer);
+
+    Ok(warp::reply::with_status("Answer added", StatusCode::OK))
+}
+
 async fn return_error(rejection: Rejection) -> Result<impl Reply, Rejection> {
     println!("{:?}", rejection);
     if let Some(error) = rejection.find::<Error>() {
