5060b354d9fd4fba4b1eb62f2194fec2dc0609e3
Gen Ichino <zenoplex@gmail.com>
2023-08-03 23:50:04 +0900

    refactor: move extract_pagination

--- src/types/pagination.rs
+++ src/types/pagination.rs
@@ -1,5 +1,27 @@
+use crate::error;
+use std::collections::HashMap;
+
 #[derive(Debug)]
 pub struct Pagination {
     pub start: usize,
     pub end: usize,
+}
+
+pub fn extract_pagination(params: HashMap<String, String>) -> Result<Pagination, error::Error> {
+    if params.contains_key("start") && params.contains_key("end") {
+        let start = params
+            .get("start")
+            .unwrap()
+            .parse::<usize>()
+            .map_err(error::Error::ParseError)?;
+        let end = params
+            .get("end")
+            .unwrap()
+            .parse::<usize>()
+            .map_err(error::Error::ParseError)?;
+
+        return Ok(Pagination { start, end });
+    }
+
+    Err(error::Error::MissingParameters)
 }
