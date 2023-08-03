2f643023ed804909f1d9bca873ce3e875a1a3a19
Gen Ichino <zenoplex@gmail.com>
2023-08-03 23:52:57 +0900

    refactor: move answer controller

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,9 @@
-use std::collections::HashMap;
-use warp::{http::Method, http::StatusCode, Filter, Rejection, Reply};
+use warp::{http::Method, Filter};
 
 mod error;
 mod routes;
 mod store;
 mod types;
-
-use crate::types::{
-    answer::{Answer, AnswerId},
-    question::QuestionId,
-};
-
-async fn add_answer(
-    store: store::Store,
-    params: HashMap<String, String>,
-) -> Result<impl Reply, Rejection> {
-    let answer = Answer {
-        id: AnswerId("1".to_string()),
-        // TODO: Stop using unwrap
-        content: params.get("content").unwrap().to_string(),
-        question_id: QuestionId(params.get("question_id").unwrap().to_string()),
-    };
-
-    store
-        .answers
-        .write()
-        .await
-        .insert(answer.id.clone(), answer);
-
-    Ok(warp::reply::with_status("Answer added", StatusCode::OK))
-}
 
 #[tokio::main]
 async fn main() {
@@ -82,7 +56,7 @@ async fn main() {
         .and(warp::path::end())
         .and(store_filter.clone())
         .and(warp::body::form())
-        .and_then(add_answer);
+        .and_then(routes::answer::add_answer);
 
     let routes = get_questions
         .or(get_question)
--- src/routes/answer.rs
+++ src/routes/answer.rs
@@ -1,1 +1,25 @@
+use crate::store;
+use crate::types::answer::{Answer, AnswerId};
+use crate::types::question::QuestionId;
+use std::collections::HashMap;
+use warp::{http::StatusCode, Rejection, Reply};
 
+pub async fn add_answer(
+    store: store::Store,
+    params: HashMap<String, String>,
+) -> Result<impl Reply, Rejection> {
+    let answer = Answer {
+        id: AnswerId("1".to_string()),
+        // TODO: Stop using unwrap
+        content: params.get("content").unwrap().to_string(),
+        question_id: QuestionId(params.get("question_id").unwrap().to_string()),
+    };
+
+    store
+        .answers
+        .write()
+        .await
+        .insert(answer.id.clone(), answer);
+
+    Ok(warp::reply::with_status("Answer added", StatusCode::OK))
+}
