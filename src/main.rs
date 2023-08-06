bfdb09ebeb5a33f62a7ae1324aaa0894cab8db4e
Gen Ichino <zenoplex@gmail.com>
2023-08-07 00:48:26 +0900

    refactor: add more log info

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,15 @@ async fn main() {
     log::warn!("This is a warning!");
 
     let log = warp::log::custom(|info| {
-        eprintln!("{} {} {}", info.method(), info.path(), info.status());
+        eprintln!(
+            "{} {} {} {:?} from {} with {:?}",
+            info.method(),
+            info.path(),
+            info.status(),
+            info.elapsed(),
+            info.remote_addr().unwrap(),
+            info.request_headers()
+        );
     });
 
     let store = store::Store::new();
