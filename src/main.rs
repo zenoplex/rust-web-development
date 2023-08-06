6f72bb432778e85af6b1b4a508e2209444757510
Gen Ichino <zenoplex@gmail.com>
2023-08-06 23:55:37 +0900

    chore: test logger

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,12 @@ mod types;
 
 #[tokio::main]
 async fn main() {
+    env_logger::init();
+
+    log::error!("This is an error!");
+    log::info!("This is info!");
+    log::warn!("This is a warning!");
+
     let store = store::Store::new();
     let store_filter = warp::any().map(move || store.clone());
 
