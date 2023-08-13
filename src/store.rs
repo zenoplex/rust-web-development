57c881dcfe07d173627b88d031c7492515200a03
Gen Ichino <zenoplex@gmail.com>
2023-08-14 01:27:35 +0900

    fix: fix sql

--- src/store.rs
+++ src/store.rs
@@ -31,18 +31,22 @@ impl Store {
         limit: Option<u32>,
         offset: u32,
     ) -> Result<Vec<Question>, Error> {
-        match sqlx::query("SELECT * from answers LIMIT $1 OFFSET $2")
-            // sqlx expects i32, but we want to use u32
-            .bind(limit.map(|u| u as i32))
-            .bind(offset as i32)
-            .map(|row: PgRow| Question {
-                id: QuestionId(row.get("id")),
-                title: row.get("title"),
-                content: row.get("content"),
-                tags: row.get("tags"),
-            })
-            .fetch_all(&self.connection)
-            .await
+        match sqlx::query(
+            "SELECT * from questions
+            LIMIT $1
+            OFFSET $2",
+        )
+        // sqlx expects i32, but we want to use u32
+        .bind(limit.map(|u| u as i32))
+        .bind(offset as i32)
+        .map(|row: PgRow| Question {
+            id: QuestionId(row.get("id")),
+            title: row.get("title"),
+            content: row.get("content"),
+            tags: row.get("tags"),
+        })
+        .fetch_all(&self.connection)
+        .await
         {
             Ok(questions) => Ok(questions),
             Err(e) => {
@@ -53,18 +57,22 @@ impl Store {
     }
 
     pub async fn add_question(&self, new_question: NewQuestion) -> Result<Question, Error> {
-        match sqlx::query("INSERT INTO questions (title, content, tags) VALUES ($1 $2 $3)")
-            .bind(new_question.title)
-            .bind(new_question.content)
-            .bind(new_question.tags)
-            .map(|row: PgRow| Question {
-                id: QuestionId(row.get("id")),
-                title: row.get("title"),
-                content: row.get("content"),
-                tags: row.get("tags"),
-            })
-            .fetch_one(&self.connection)
-            .await
+        match sqlx::query(
+            "INSERT INTO questions (title, content, tags)
+            VALUES ($1, $2, $3)
+            RETURNING id, title, content, tags",
+        )
+        .bind(new_question.title)
+        .bind(new_question.content)
+        .bind(new_question.tags)
+        .map(|row: PgRow| Question {
+            id: QuestionId(row.get("id")),
+            title: row.get("title"),
+            content: row.get("content"),
+            tags: row.get("tags"),
+        })
+        .fetch_one(&self.connection)
+        .await
         {
             Ok(question) => Ok(question),
             Err(e) => {
@@ -79,19 +87,23 @@ impl Store {
         question: Question,
         question_id: i32,
     ) -> Result<Question, Error> {
-        match sqlx::query("UPDATE questions SET title = $1, content = $2, tags = $3 WHERE id = $4")
-            .bind(question.title)
-            .bind(question.content)
-            .bind(question.tags)
-            .bind(question_id)
-            .map(|row: PgRow| Question {
-                id: QuestionId(row.get("id")),
-                title: row.get("title"),
-                content: row.get("content"),
-                tags: row.get("tags"),
-            })
-            .fetch_one(&self.connection)
-            .await
+        match sqlx::query(
+            "UPDATE questions SET title = $1, content = $2, tags = $3
+            WHERE id = $4
+            RETURNING id, title, content, tags",
+        )
+        .bind(question.title)
+        .bind(question.content)
+        .bind(question.tags)
+        .bind(question_id)
+        .map(|row: PgRow| Question {
+            id: QuestionId(row.get("id")),
+            title: row.get("title"),
+            content: row.get("content"),
+            tags: row.get("tags"),
+        })
+        .fetch_one(&self.connection)
+        .await
         {
             Ok(question) => Ok(question),
             Err(e) => {
@@ -116,16 +128,19 @@ impl Store {
     }
 
     pub async fn add_answer(&self, new_answer: NewAnswer) -> Result<Answer, Error> {
-        match sqlx::query("INSERT INTO answers (content, question_id) VALUES ($1 $2)")
-            .bind(new_answer.content)
-            .bind(new_answer.question_id.0)
-            .map(|row: PgRow| Answer {
-                id: AnswerId(row.get("id")),
-                content: row.get("content"),
-                question_id: QuestionId(row.get("question_id")),
-            })
-            .fetch_one(&self.connection)
-            .await
+        match sqlx::query(
+            "INSERT INTO answers (content, question_id)
+            VALUES ($1, $2)",
+        )
+        .bind(new_answer.content)
+        .bind(new_answer.question_id.0)
+        .map(|row: PgRow| Answer {
+            id: AnswerId(row.get("id")),
+            content: row.get("content"),
+            question_id: QuestionId(row.get("question_id")),
+        })
+        .fetch_one(&self.connection)
+        .await
         {
             Ok(answer) => Ok(answer),
             Err(e) => {
