40428b47eb6624531fd82c1357d1460dcc6af038
Gen Ichino <zenoplex@gmail.com>
2023-08-19 22:59:15 +0900

    fix: add returning row

--- src/store.rs
+++ src/store.rs
@@ -129,15 +129,16 @@ impl Store {
 
     pub async fn add_answer(&self, new_answer: NewAnswer) -> Result<Answer, Error> {
         match sqlx::query(
-            "INSERT INTO answers (content, question_id)
-            VALUES ($1, $2)",
+            "INSERT INTO answers (content, corresponding_question)
+            VALUES ($1, $2)
+            RETURNING id, content, corresponding_question",
         )
         .bind(new_answer.content)
         .bind(new_answer.question_id.0)
         .map(|row: PgRow| Answer {
             id: AnswerId(row.get("id")),
             content: row.get("content"),
-            question_id: QuestionId(row.get("question_id")),
+            question_id: QuestionId(row.get("corresponding_question")),
         })
         .fetch_one(&self.connection)
         .await
