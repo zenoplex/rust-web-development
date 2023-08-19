037b331cdf75438099d483ea48bb79c35ce56a18
Gen Ichino <zenoplex@gmail.com>
2023-08-19 22:59:00 +0900

    feat: add profanity check to add_answer

--- src/routes/answer.rs
+++ src/routes/answer.rs
@@ -1,3 +1,4 @@
+use crate::profanity;
 use crate::store;
 use crate::types::answer::NewAnswer;
 use warp::{http::StatusCode, Rejection, Reply};
@@ -6,9 +7,18 @@ pub async fn add_answer(
     store: store::Store,
     new_answer: NewAnswer,
 ) -> Result<impl Reply, Rejection> {
-    if let Err(e) = store.add_answer(new_answer).await {
-        return Err(warp::reject::custom(e));
-    }
+    let content = match profanity::check_profanity(new_answer.content).await {
+        Ok(res) => res,
+        Err(e) => return Err(warp::reject::custom(e)),
+    };
 
-    Ok(warp::reply::with_status("Answer added", StatusCode::OK))
+    let answer = NewAnswer {
+        content,
+        question_id: new_answer.question_id,
+    };
+
+    match store.add_answer(answer).await {
+        Ok(_) => Ok(warp::reply::with_status("Answer added", StatusCode::OK)),
+        Err(e) => Err(warp::reject::custom(e)),
+    }
 }
