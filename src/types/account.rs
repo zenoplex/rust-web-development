7c1291a2e4ebcc224138f86ae49085c702683637
Gen Ichino <zenoplex@gmail.com>
2023-08-20 23:55:09 +0900

    feat: add id to Account

--- src/types/account.rs
+++ src/types/account.rs
@@ -1,7 +1,17 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
+struct AccountId(i32);
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
 struct Account {
+    id: AccountId,
+    email: String,
+    password: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct NewAccount {
     email: String,
     password: String,
 }
