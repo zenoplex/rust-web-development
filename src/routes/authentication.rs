7cdabfd47c749f1b2037a06e61a79e0ca96bf1fb
Gen Ichino <zenoplex@gmail.com>
2023-08-22 01:53:48 +0900

    feat: add hash_password fn

--- src/routes/authentication.rs
+++ src/routes/authentication.rs
@@ -1,3 +1,5 @@
+use argon2::{self, Config};
+use rand::Rng;
 use warp::{Rejection, Reply};
 
 use crate::{store::Store, types::account::Account};
@@ -7,4 +9,10 @@ pub async fn register(store: Store, account: Account) -> Result<impl Reply, Reje
         Ok(account) => Ok(warp::reply::json(&account)),
         Err(e) => Err(warp::reject::custom(e)),
     }
+}
+
+fn hash_password(password: &[u8]) -> String {
+    let salt = rand::thread_rng().gen::<[u8; 32]>();
+    let config = Config::default();
+    argon2::hash_encoded(password, &salt, &config).unwrap()
 }
