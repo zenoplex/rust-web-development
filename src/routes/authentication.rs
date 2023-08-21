ab0f0d68288a636c8d362686dac913bc4320db3f
Gen Ichino <zenoplex@gmail.com>
2023-08-22 02:01:00 +0900

    feat: hash & salt password

--- src/routes/authentication.rs
+++ src/routes/authentication.rs
@@ -5,6 +5,13 @@ use warp::{Rejection, Reply};
 use crate::{store::Store, types::account::Account};
 
 pub async fn register(store: Store, account: Account) -> Result<impl Reply, Rejection> {
+    let hashed_password = hash_password(account.password.as_bytes());
+
+    let account = Account {
+        password: hashed_password,
+        ..account
+    };
+
     match store.add_account(account).await {
         Ok(account) => Ok(warp::reply::json(&account)),
         Err(e) => Err(warp::reject::custom(e)),
