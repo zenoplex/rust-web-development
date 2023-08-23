24756af07ff75ee08675a65aaf1d4286ffd490e0
Gen Ichino <zenoplex@gmail.com>
2023-08-24 00:52:38 +0900

    style: format

--- src/main.rs
+++ src/main.rs
@@ -96,7 +96,7 @@ async fn main() {
         .and(warp::body::json())
         .and_then(routes::authentication::register);
 
-     let login = warp::post()
+    let login = warp::post()
         .and(warp::path("login"))
         .and(warp::path::end())
         .and(store_filter.clone())
