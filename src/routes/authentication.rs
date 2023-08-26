c22cb663480444077659c8265c270f8ca6552c09
Gen Ichino <zenoplex@gmail.com>
2023-08-26 16:29:04 +0900

    feat: return token on login

--- src/routes/authentication.rs
+++ src/routes/authentication.rs
@@ -1,8 +1,12 @@
 use argon2::{self, Config};
+use paseto::v2::local_paseto;
 use rand::Rng;
 use warp::{Rejection, Reply};
 
-use crate::{store::Store, types::account::Account};
+use crate::{
+    store::Store,
+    types::account::{Account, AccountId},
+};
 
 pub async fn register(store: Store, account: Account) -> Result<impl Reply, Rejection> {
     let hashed_password = hash_password(account.password.as_bytes());
@@ -29,7 +33,9 @@ pub async fn login(store: Store, login: Account) -> Result<impl Reply, Rejection
         Ok(account) => match verify_password(&account.password, login.password.as_bytes()) {
             Ok(verified) => {
                 if verified {
-                    Ok(warp::reply::json(&account))
+                    Ok(warp::reply::json(&issue_token(
+                        account.id.expect("Account id not found"),
+                    )))
                 } else {
                     Err(warp::reject::custom(
                         handle_error::Error::WrongPasswordError,
@@ -46,4 +52,12 @@ pub async fn login(store: Store, login: Account) -> Result<impl Reply, Rejection
 
 fn verify_password(hash: &str, password: &[u8]) -> Result<bool, argon2::Error> {
     argon2::verify_encoded(hash, password)
+}
+
+const PASETO_KEY: [u8; 32] = [0u8; 32];
+
+fn issue_token(account_id: AccountId) -> String {
+    let state = serde_json::to_string(&account_id).expect("Failed to serialize");
+
+    local_paseto(&state, None, &PASETO_KEY).expect("Failed to create token")
 }
